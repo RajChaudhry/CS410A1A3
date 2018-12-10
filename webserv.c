f5cc3c15623a23d8f532651c1a800a4d2f4957ee
rchaud <rchaud@168.122.142.56>
2018-12-10 16:58:39 -0500

    initial

---
+++ webserv.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   webserv.c
+ * Author: rchaud
+ *
+ * Created on December 10, 2018, 4:31 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <termios.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
